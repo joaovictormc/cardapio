209531f099daad7e9fcd1173906f942abeb7efd7
João Victor Maciel <62820876+joaovictormc@users.noreply.github.com>
2024-04-19 11:52:45 -0300

    finalizado

--- frontend/cardapio/src/components/card/create-modal/create-modat.tsx
+++ frontend/cardapio/src/components/card/create-modal/create-modat.tsx
@@ -1,29 +1,61 @@
+import { useState } from "react"
+import { useFoodDataMutate } from "../../../hooks/useFoodDataMutate";
+import { FoodData } from "../../../interface/FoodData";
+import "./modal.css";
+
 interface InputProps {
     label:string,
     value: string | number,
-    updateValue(value: string | number): void
+    updateValue(value: any): void
 }
+
+// interface ModalProps {
+//     closeModal(): void
+// }
 
 
 const Input = ({label, value, updateValue} : InputProps) => {
     return (
         <>
             <label>{label}</label>
-            <input value={value} onChange={event => updateValue(event.target.value)} />
+            <input value={value} onChange={event => updateValue(event.target.value)}></input>
         </>
     )
 }
 
-export function CreateModal() {
+export function CreateModal(/*{closeModal}: ModalProps*/) {
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState(0);
+    const [image, setImage] = useState("");
+    const { mutate/*, isSuccess*/ } = useFoodDataMutate();
+
+    const submit = () => {
+        const foodData: FoodData = {
+            title,
+            price,
+            image
+        }
+        mutate(foodData)
+    }
+
+    // useEffect(() => {
+    //     if(!isSuccess) return
+    //         closeModal();
+    // }, [isSuccess])
+
     return(
-        <div className="modal-overflow">
+        <div className="modal-overlay">
             <div className="modal-body">
                 <h2>Cadastre um novo item no cardápio</h2>
-                <form className="input-container" action="">
-                    <Input label="title" value={} updateValue={}/>
-                    <Input label="price" value={} updateValue={}/>
-                    <Input label="image" value={} updateValue={}/>
+                <form className="input-container">
+                    <Input label="Nome do Prato" value={title} updateValue={setTitle}/>
+                    <Input label="Preço" value={price} updateValue={setPrice}/>
+                    <Input label="Imagem" value={image} updateValue={setImage}/>
                 </form>
+
+                <button onClick={submit} className="btn-secondary">
+                    Postar
+                </button>
             </div>
         </div>
     )
